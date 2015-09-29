9b2223c4c358b6968f76b1345e5af6fa184bfc31
David Aghassi <david.aghassi@gmail.com>
2015-09-28 22:33:08 -0400

    Initial Commit

---
+++ findmybusnj/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  findmybusnj
+//
+//  Created by David Aghassi on 9/28/15.
+//  Copyright © 2015 David Aghassi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
