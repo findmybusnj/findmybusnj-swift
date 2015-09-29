bfbbfa9bb43ad29843b6e5d27dfff26396292ef9
David Aghassi <david.aghassi@gmail.com>
2015-09-28 23:13:40 -0400

    Added embedded navigation controller
    -  Renamed controllers
    -  embedded tables in navigation controllers

--- findmybusnj/BusTimeTableController.swift
+++ findmybusnj/BusTimeTableController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class BusTimeTableController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- findmybusnj/FindByBusNumberController.swift
+++ findmybusnj/FindByBusNumberController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class FindByBusNumberController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
