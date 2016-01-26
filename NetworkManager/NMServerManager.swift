97d873f1b05da73e50ecfeca6fa1485febe5741f
David Aghassi <david.aghassi@gmail.com>
2016-01-26 11:57:19 -0500

    Started adding filterByBus function
    -  Wrote out getJSONForStopFilteredByBus function
    for when user passes in a bus to filter
    -  created makePost function to factor out common
    code

--- NetworkManager/NMServerManager.swift
+++ NetworkManager/NMServerManager.swift
@@ -9,27 +9,61 @@
 
 import Foundation
 
+// MARK: Dependencies
 import Alamofire
 import SwiftyJSON
 
+/**
+ Part of the `NetworkManager` framework. Used to make calls to findmybusnj.com server
+*/
 public class NMServerManager {
     private static let baseURL = "https://findmybusnj.com/rest"
     
+    /**
+     Get all the buses coming to a given stop. When using this, you need to pass a completion function in to handle the json returned by the server call.
+     
+     - Parameters:
+        - stop: The six digit stop number that the user gets from myBus sign at stop
+        - completion: A function to be called back upon success that takes a JSON array and any errors
+    */
     public static func getJSONForStop(stop: String, completion: (item: JSON, error: ErrorType?) -> Void) {
-        let endpoint = baseURL + "/stop"
-        
         let parameters = [ "stop" : stop ]
-        Alamofire.request(.POST, endpoint, parameters: parameters)
+        makePost("/stop", parameters: parameters, completion: completion)
+    }
+    
+    /**
+     Get all the buses coming to a given stop, filtered by the bus number. So if the user passes in `165`, the buses returned will only be those that have `165` in the number scheme. This function requires a completion handler so that the json can be handled when called.
+     
+     - Parameters:
+        - stop: The six digit string provided by the myBus stop sign that the user types in
+        - bus: The three digit string that defines the bus number to filter on
+        - completion: A callback function to handle the JSON data upon a successful request.
+    */
+    public static func getJSONForStopFilteredByBus(stop: String, bus: String, completion: (item: JSON, error: ErrorType?) -> Void) {
+        let parameters = [ "stop" : stop, "bus" : bus]
+        makePost("/stopFilteredByBus", parameters: parameters, completion: completion);
+    }
+    
+    /**
+     Wrapper function around common `.POST` request call. Used for making post requests that take a string of parameters to go to an endpoint. This function requires a completion handler to handle the JSON once it responds successfully.
+     
+     - Parameters:
+    */
+    private static func makePost(endpoint: String, parameters: [String: String], completion: (item: JSON, error: ErrorType?) -> Void) {
+        let url = baseURL + endpoint
+        
+        Alamofire.request(.POST, url, parameters: parameters)
             .responseJSON {(req, res, json) in
                 if (json.isFailure) {
-                    NSLog("Error: \(json.error)")
+                    print("Error: \(json.error)")
                 }
                 else {
+                    // we know we can force case since it isn't a failure
                     let json = JSON(json.value!)
+                    
                     // call closure for what is past in (kind of like an anonymous function)
-                    completion(item: json, error: nil);
+                    completion(item: json, error: nil)
                 }
         }
     }
-    
 }
