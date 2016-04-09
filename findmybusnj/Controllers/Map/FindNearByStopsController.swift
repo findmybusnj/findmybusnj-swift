cf961dd24834f200e22034819cb1e4ad0dffa7c2
David Aghassi <david.aghassi@gmail.com>
2016-04-09 15:22:58 -0400

    Deleted extension file, moved it to original file
    -  Moved delegate class to class definition
    -  Animated pin drops
    -  Added calls for checking what mode of debugging
    we are in

--- findmybusnj/Controllers/Map/FindNearByStopsController.swift
+++ findmybusnj/Controllers/Map/FindNearByStopsController.swift
@@ -27,15 +27,11 @@ class FindNearByStopsController: UIViewController {
     super.viewDidLoad()
     mapView.delegate = self
     
-    #if RELEASE
+    #if RELEASE || TESTFLIGHT
       centerMapOnLocation()
     #endif
-      queryPlaces("bus_station")
-  }
-  
-  override func viewWillAppear(animated: Bool) {
-    super.viewDidAppear(animated)
-    checkLocationAuthorizationStatus()
+    
+    queryPlaces("bus_station")
   }
   
   /**
@@ -50,18 +46,19 @@ class FindNearByStopsController: UIViewController {
   /**
    Takes in a string that denotes the type of place to query google for,
    Sends a `.POST` request to the endpoint, which hits google, and returns JSON
+   This method needs to be moved to the `NetworkManager` eventually
    
-   - Parameter googleType: A string that represents the google types. You can find more
+   - parameter googleType: A string that represents the google types. You can find more
    info on the type at the [Google Places API](https://developers.google.com/places/supported_types)
    */
   private func queryPlaces(googleType: String) {
-    #if RELEASE
+    #if RELEASE || TESTFLIGHT
       let coordinate = locationManager.location!.coordinate
     #endif
     
     let queryRegion = regionRadius * 3
     
-    // Test data (home)
+    // Test data
     #if DEBUG
       let latitude = 40.9445783
       let longitude = -74.1051304
@@ -82,18 +79,25 @@ class FindNearByStopsController: UIViewController {
       "radius" : String(queryRegion),
       "types" : googleType
     ]
-    print(parameters)
+    #if DEBUG
+      print(parameters)
+    #endif
     
     Alamofire.request(.POST, url, parameters: parameters).responseJSON {
-      response in
+      [unowned self] response in
       let json = response.result
       
       if (json.isFailure) {
         NSLog("Error: \(json.error)")
       }
       else {
+        // remove prior annotations if any
+        self.mapView.removeAnnotations(self.mapView.annotations)
         let json = JSON(json.value!)
-        print(json)
+        #if DEBUG
+          print(json)
+        #endif
+        
         let results = json["results"]
         var locName: String         // location name
         var latitude: Double
@@ -104,7 +108,9 @@ class FindNearByStopsController: UIViewController {
           latitude = results[i]["geometry"]["location"]["lat"].double!
           longitude = results[i]["geometry"]["location"]["lng"].double!
           
-          print(i, "bus is:",  locName, "with latitude", latitude, "and longitude", longitude)
+          #if DEBUG
+            print(i, "bus is:",  locName, "with latitude", latitude, "and longitude", longitude)
+          #endif
           let busStopAnnotation = PlacesAnnotation(title: locName,
             coordinate: CLLocationCoordinate2D(latitude: latitude, longitude: longitude))
           self.mapView.addAnnotation(busStopAnnotation)
@@ -113,6 +119,9 @@ class FindNearByStopsController: UIViewController {
     }
   }
   
+  /**
+   Checks to see if the user has authorized use of location services
+   */
   private func checkLocationAuthorizationStatus() {
     if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
       mapView.showsUserLocation = true
@@ -120,5 +129,43 @@ class FindNearByStopsController: UIViewController {
     else {
       locationManager.requestWhenInUseAuthorization()
     }
+  }
+}
+
+// MARK: MKMapViewDelegate
+extension FindNearByStopsController: MKMapViewDelegate {
+  func mapViewWillStartLoadingMap(mapView: MKMapView) {
+    checkLocationAuthorizationStatus()
+  }
+ 
+  func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+    if let annotation = annotation as? PlacesAnnotation {
+      let identifier = "pin"
+      var view: MKPinAnnotationView
+      
+      if let dequeuedView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier) as? MKPinAnnotationView {
+        dequeuedView.annotation = annotation
+        view = dequeuedView
+      }
+      else {
+        let button = UIButton(type: .DetailDisclosure)
+        button.setImage(UIImage(named: "Car"), forState: .Normal)
+        
+        view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        view.canShowCallout = true
+        view.calloutOffset = CGPoint(x: -5, y: 5)
+        view.rightCalloutAccessoryView = button
+        view.animatesDrop = true
+        return view
+      }
+    }
+    
+    return nil
+  }
+  
+  func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+    let location = view.annotation as! PlacesAnnotation
+    let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+    location.mapItem().openInMapsWithLaunchOptions(launchOptions)
   }
 }
